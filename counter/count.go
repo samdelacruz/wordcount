1d282e98191bdfdc6813f6ee2ba78ece71a188e8
Sam Delacruz <samdelacruz91@gmail.com>
2017-05-08 21:32:00 +0100

    Split words and normalize with regexp

--- counter/count.go
+++ counter/count.go
@@ -5,25 +5,15 @@ import "regexp"
 
 // FromString returns the count of each word in a string
 func FromString(text *string) map[string]int {
-	words := strings.Split(*text, " ")
+	words := regexp.MustCompile("[^a-z]+").Split(strings.ToLower(*text), -1)
 	return fromWords(&words)
-}
-
-func filterWord(word *string) *string {
-	w := strings.ToLower(*word)
-	reg, _ := regexp.Compile("[^a-z]")
-	w = reg.ReplaceAllString(w, "")
-	if w == *word {
-		return word
-	}
-	return &w
 }
 
 func fromWords(words *[]string) map[string]int {
 	counts := make(map[string]int)
 
 	for i := range *words {
-		word := filterWord(&(*words)[i])
+		word := &(*words)[i]
 		if len(*word) > 0 {
 			counts[*word]++
 		}
